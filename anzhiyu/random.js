d3d507432902a0e265bdcbf97ad114b19af63b64
wenjingdeyu <1033130252@qq.com>
2024-09-08 18:24:41 +0800

    Site updated: 2024-09-08 18:24:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/08/guideline for markdown/","2024/09/08/hello-world/","2024/09/08/微信机器人/"];function toRandomPost(){
+var posts=["2024/09/08/guideline for markdown/","2024/09/08/hello-world/","2024/09/08/微信机器人/","2024/09/08/贺新郎/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
