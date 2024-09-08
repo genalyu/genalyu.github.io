3db3c7739bf9cf27d33b52f09409c42a92eea719
wenjingdeyu <1033130252@qq.com>
2024-09-08 18:19:19 +0800

    Site updated: 2024-09-08 18:19:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/08/guideline for markdown/","2024/09/08/hello-world/"];function toRandomPost(){
+var posts=["2024/09/08/guideline for markdown/","2024/09/08/hello-world/","2024/09/08/微信机器人/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
