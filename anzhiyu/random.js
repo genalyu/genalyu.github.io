826a4e014b4009145386d7116ee4b5d584b8b27d
wenjingdeyu <1033130252@qq.com>
2024-09-24 23:31:57 +0800

    Site updated: 2024-09-24 23:31:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/08/guideline for markdown/","2024/09/08/微信机器人/","2024/09/08/贺新郎/","2024/09/11/todolist/","2024/09/11/点名软件/","2024/09/11/python爬虫下载图片/"];function toRandomPost(){
+var posts=["2024/09/08/guideline for markdown/","2024/09/08/微信机器人/","2024/09/08/贺新郎/","2024/09/11/todolist/","2024/09/11/点名软件/","2024/09/11/python爬虫下载图片/","2024/09/24/音乐播放器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
